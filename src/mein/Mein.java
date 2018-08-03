40d4536058cd497d24edea92a6344c9dfdcdae65
Nincub <nincub111@gmail.com>
2018-08-03 17:42:31 -0500

    Se crean clases abstract e interfaces

--- src/mein/Mein.java
+++ src/mein/Mein.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dogos;
+package mein;
 
 /**
  *
  * @author Nincub
  */
-public class Dogos {
+public class Mein {
 
     /**
      * @param args the command line arguments
