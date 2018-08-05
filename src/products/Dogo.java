953ea6161e5733b5ea5592de1d18b8dda3209702
Edgar <hilkser@hotmail.com>
2018-08-05 17:20:43 -0500

    Se agregan métodos en las clases salchica y dogo

--- src/products/Dogo.java
+++ src/products/Dogo.java
@@ -5,10 +5,14 @@
  */
 package products;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.LinkedList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -72,22 +76,125 @@ public class Dogo extends abstracts.AProducto {
 
     @Override
     public Object Leer() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        LinkedList<Dogo> ll;
+        try {
+            FileInputStream in = new FileInputStream("Dogo");
+            ObjectInputStream ois = new ObjectInputStream(in);
+            ll = new LinkedList();
+            while(ois.read() != -1) {
+                ll.add((Dogo)ois.readObject());
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Dogo.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        } catch (IOException | ClassNotFoundException ex) {
+            Logger.getLogger(Dogo.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+        return ll;
     }
 
     @Override
     public boolean Modificar(Object obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        File F = new File("Dogo");
+        File F2 = new File ("Dogo1");
+        boolean ban = false;
+        Dogo aux;
+        LinkedList<Dogo> ll = (LinkedList) obj;
+        Dogo dogo = (Dogo) ll.pollFirst();
+        Dogo dogo1 = (Dogo) ll.pollLast();
+        if(pan == null || dogo1 == null){
+            if (!pan.equals(dogo1)) {
+                System.err.println("No puede ser igual el pan a modificar");
+            } else {
+                System.err.println("lista de panes a modificar incompleta");
+            }
+            return ban;
+        }
+        try {
+            FileInputStream in = new FileInputStream(F);
+            ObjectInputStream ois = new ObjectInputStream(in);
+            FileOutputStream out = new FileOutputStream(F2, true);
+            ObjectOutputStream oos = new ObjectOutputStream(out);
+            while (ois.read() != -1) {
+                aux = (Dogo) ois.readObject();
+                if (aux.equals(dogo)){
+                    oos.writeObject(dogo1);
+                    ban = true;
+                } else {
+                    oos.writeObject(aux);
+                }
+            }
+            in.close();
+            oos.close();
+            if (F.delete()) {
+                if (F2.renameTo(F)) {
+                    System.out.println("Exito");
+                }
+            }  
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Dogo.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException | ClassNotFoundException ex) {
+            Logger.getLogger(Dogo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return ban;
     }
 
     @Override
     public boolean Eliminar(Object obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        boolean ban = false;
+        File F = new File("Dogo");
+        File F2 = new File("Dogo1");
+        Dogo aux;
+        try {
+            FileInputStream in = new FileInputStream(F);
+            ObjectInputStream ois = new ObjectInputStream(in);
+            FileOutputStream out = new FileOutputStream(F2, true);
+            ObjectOutputStream oos = new ObjectOutputStream(out);
+            while (ois.read() != -1){
+                aux = (Dogo) ois.readObject();
+                if (aux.equals((Dogo)obj)) {
+                    ban = true;
+                } else {
+                    oos.writeObject(aux);
+                }
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Dogo.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Dogo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return ban;
     }
 
     @Override
     public boolean EliminarLogic(Object obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+     boolean ban = false;
+        File F = new File("Dogo");
+        File F2 = new File("Dogo1");
+        Dogo aux;
+        try {
+            FileInputStream in = new FileInputStream(F);
+            ObjectInputStream ois = new ObjectInputStream(in);
+            FileOutputStream out = new FileOutputStream(F2, true);
+            ObjectOutputStream oos = new ObjectOutputStream(out);
+            while (ois.read() != -1){
+                aux = (Dogo) ois.readObject();
+                if (aux.equals((Dogo)obj)) {
+                    aux.setStatus(false);
+                    oos.writeObject(aux);
+                    ban = true;
+                } else {
+                    oos.writeObject(aux);
+                }
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Dogo.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Dogo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return ban;
+        
     }
 
     /**
--- src/products/Salchicha.java
+++ src/products/Salchicha.java
@@ -5,6 +5,17 @@
  */
 package products;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Hilxer
@@ -17,7 +28,7 @@ public class Salchicha  extends abstracts.AProducto {
        super(precio,nombre, status);
        
     }
-      
+    @Deprecated 
     @Override
     public void calcularPrecio() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -25,32 +36,149 @@ public class Salchicha  extends abstracts.AProducto {
 
     @Override
     public String toString() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return "{Salchicha{Nombre:" + this.getNombre() + ",Precio:" + this.getPrecio() +"}}";
     }
 
     @Override
     public boolean Escribir(Object obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      boolean exito = true;
+        try {
+            FileOutputStream hola = new FileOutputStream("Salchicha", true);
+            ObjectOutputStream hi = new ObjectOutputStream(hola);
+            hi.writeObject(obj);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Salchicha.class.getName()).log(Level.SEVERE, null, ex);
+            exito = false;
+        } catch (IOException ex) {
+            Logger.getLogger(Salchicha.class.getName()).log(Level.SEVERE, null, ex);
+            exito = false;
+        } catch (Exception ex) {
+            System.err.println(ex);
+            exito = false;
+        }
+        return exito;
     }
 
     @Override
     public Object Leer() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         LinkedList<Salchicha> aaa;
+        try {
+            FileInputStream eee = new FileInputStream("Salchicha");
+            ObjectInputStream ole = new ObjectInputStream(eee);
+            aaa = new LinkedList();
+            while(ole.read() != -1) {
+                aaa.add((Salchicha)ole.readObject());
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Salchicha.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        } catch (IOException | ClassNotFoundException ex) {
+            Logger.getLogger(Salchicha.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+        return aaa;
     }
 
     @Override
     public boolean Modificar(Object obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        File F = new File("Salchicha");
+        File F2 = new File ("Salchicha1");
+        boolean ban = false;
+        Salchicha aux;
+        LinkedList<Salchicha> aaa = (LinkedList) obj;
+        Salchicha salchicha = (Salchicha) aaa.pollFirst();
+        Salchicha salchicha1 = (Salchicha) aaa.pollLast();
+        if(salchicha == null || salchicha1 == null){
+            if (!salchicha.equals(salchicha1)) {
+                System.err.println("No puede ser igual la salchicha a modificar");
+            } else {
+                System.err.println("lista de salchichas a modificar incompleta");
+            }
+            return ban;
+        }
+        try {
+            FileInputStream eee = new FileInputStream(F);
+            ObjectInputStream ole = new ObjectInputStream(eee);
+            FileOutputStream hola = new FileOutputStream(F2, true);
+            ObjectOutputStream hi = new ObjectOutputStream(hola);
+            while (ole.read() != -1) {
+                aux = (Salchicha) ole.readObject();
+                if (aux.equals(salchicha)){
+                    hi.writeObject(salchicha1);
+                    ban = true;
+                } else {
+                    hi.writeObject(aux);
+                }
+            }
+            eee.close();
+            hi.close();
+            if (F.delete()) {
+                if (F2.renameTo(F)) {
+                    System.out.println("Exito");
+                }
+            }  
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Salchicha.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException | ClassNotFoundException ex) {
+            Logger.getLogger(Salchicha.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return ban;
     }
 
     @Override
     public boolean Eliminar(Object obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       boolean ban = false;
+        File F = new File("Salchicha");
+        File F2 = new File("Salchicha1");
+        Salchicha aux;
+        try {
+            FileInputStream eee = new FileInputStream(F);
+            ObjectInputStream ole = new ObjectInputStream(eee);
+            FileOutputStream hola = new FileOutputStream(F2, true);
+            ObjectOutputStream hi = new ObjectOutputStream(hola);
+            while (ole.read() != -1){
+                aux = (Salchicha) ole.readObject();
+                if (aux.equals((Salchicha)obj)) {
+                    ban = true;
+                } else {
+                    hi.writeObject(aux);
+                }
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Salchicha.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Salchicha.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return ban;
     }
 
     @Override
     public boolean EliminarLogic(Object obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        boolean ban = false;
+        File F = new File("Salchicha");
+        File F2 = new File("Salchicha1");
+        Salchicha aux;
+        try {
+            FileInputStream eee = new FileInputStream(F);
+            ObjectInputStream ole = new ObjectInputStream(eee);
+            FileOutputStream hola = new FileOutputStream(F2, true);
+            ObjectOutputStream hi = new ObjectOutputStream(hola);
+            while (ole.read() != -1){
+                aux = (Salchicha) ole.readObject();
+                if (aux.equals((Salchicha)obj)) {
+                    aux.setStatus(false);
+                    hi.writeObject(aux);
+                    ban = true;
+                } else {
+                    hi.writeObject(aux);
+                }
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Salchicha.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Salchicha.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return ban;
     }
     
     
